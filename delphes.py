51efbc2a7bc94f515f7dbdff31322ab8c5f3ede7
Vishnu Sangli <vsangli@berkeley.edu>
2021-10-21 17:50:17 -0700

    gbba background viewed

--- delphes.py
+++ delphes.py
@@ -16,23 +16,29 @@ t.Show()
 # %% Loop over all jets and create an average image
 
 # Create an empty image corresponding to eta/phi
-etamin=-2.5 
-etamax= 2.5
+check = []
+etamin=-1.5
+etamax= 1.5
 etabin=100
 etawdt=(etamax-etamin)/etabin
 
-phimin=-3.14
-phimax=3.14
+phimin=-1.5
+phimax=1.5
 phibin=100
 phiwdt=(phimax-phimin)/phibin
 
-image=np.zeros((etabin,phibin), dtype=float)
-
+image = np.zeros((etabin,phibin), dtype=float)
+image0 = np.zeros((etabin,phibin), dtype=float)
+image1 = np.zeros((etabin,phibin), dtype=float)
+image2 = np.zeros((etabin,phibin), dtype=float)
+image3 = np.zeros((etabin,phibin), dtype=float)
+#%%
 # Loop over all event
+a = 0
 n=0
 for e in t:
     # Loop over all jets in the event
-    for fj in t.GenFatJet:
+    for fj in e.GenFatJet:
         ## Homework 2
         # Add labelling information based on:
         # 0: contains a higgs boson and both b-quarks
@@ -49,21 +55,39 @@ for e in t:
         #
         # The PID contains the PDG ID of the particle (aka the type) using the
         # scheme at https://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf
-        #
         # The Status contains the status in the generator. Use status 23 for the
-        # b-quarks to select ones from the Feynman diagram.
-        #
-        # Use different samples to determine what particle the b / light quarks came
-        # came from.
+        # b-quarks. Use status 43(confirm?) for the gluon.
+        flag = [1, 2, 5, -5, 21, 35]
+        flag = {a: 0 for a in flag}
+
+        for p in t.Particle:
+            print(p.ClassName())
+            print(dir(p))
+            assert(0> 1), "Exit Point"
+            if p.Status != 23:
+                continue
+            delt_r = np.sqrt(np.power(p.Phi - fj.Phi, 2) + np.power(p.Eta - fj.Eta, 2))
+            if delt_r < 0.5:
+                if p.PID in flag.keys():
+                    flag[p.PID] += 1
+        
+        img_use = image3
+        if flag[5] and flag[-5] and flag[35]: #Guessing the order takes precedence. This exclusivity of image objects would otherwise allow same jet to be classified in different ones
+            img_use = image0
+        if flag[5] and flag[-5] and flag[21]:
+            img_use = image1
+        if flag[21] and flag[1] and flag[2]:
+            img_use = image2        
+        a += 1
 
         # Loop over all particles in the jet
         for c in fj.Constituents:
             ## Homework 1
             # Change this to be the distance from the fat jet center.
             # ie: delta eta = c.Eta - fj.Eta
-            myeta=int(np.floor((c.Eta-etamin)/etawdt))
-            myphi=int(np.floor((c.Phi-phimin)/phiwdt))
-
+            myeta= int(np.floor((c.Eta - fj.Eta - etamin)/etawdt)) 
+            myphi= int(np.floor((c.Phi - fj.Phi - phimin)/phiwdt)) 
+            check.append([myeta, myphi, c.PT])
             # Bounds check
             if myeta < 0 or myeta >= image.shape[0]:
                 continue
@@ -71,17 +95,25 @@ for e in t:
                 continue
 
             # Add to image
-            image[myeta,myphi] = c.PT
+            image[myeta,myphi] += c.PT
+            img_use[myeta, myphi] += c.PT
         n+=1
 
 # average the image
 image/=n
-
 # %% Show the image of the average jet shape
 ## Homework 3
 # Make a separate plot for the three categories
-plt.imshow(np.log10(image),extent=(etamin,etamax,phimin,phimax))
+plt.imshow(image,extent=(etamin,etamax,phimin,phimax))
 plt.xlabel('$\eta$')
 plt.ylabel('$\phi$')
-
+plt.colorbar()
+plt.show()
+# %%
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+ax1.imshow(image0,extent=(etamin,etamax,phimin,phimax))
+ax2.imshow(image1,extent=(etamin,etamax,phimin,phimax))
+ax3.imshow(image2,extent=(etamin,etamax,phimin,phimax))
+ax4.imshow(image3,extent=(etamin,etamax,phimin,phimax))
+plt.show()
 # %%
